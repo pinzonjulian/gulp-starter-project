24e9ce1321dc31e07191af61d72311cdf3fadec2
pinzonjulian <julianpinzoneslava@gmail.com>
2016-03-31 22:09:06 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// var notify = require('gulp-notify');
+
+var runSequence = require('run-sequence');
+
+gulp.task('sass', function (){
+  return gulp.src('app/sass/main.sass')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  // .pipe(cssnano())
+  .pipe(gulp.dest('app/sass'))
+  .pipe(browserSync.stream());
+})
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/sass/**/*.sass", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
